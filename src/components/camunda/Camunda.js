ed321c0995ddfb3228cf1666b65251aca3590f10
Milan Miljus <milan.997@hotmail.com>
2019-02-23 17:41:50 +0100

    Tasklist izbacen, sad workspace

--- src/components/camunda/Camunda.js
+++ src/components/camunda/Camunda.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import TaskList from './tasklist/TaskList';
+import axios from 'axios';
+
+
 import WorkSpace from './workspace/WorkSpace';
+import TaskItem from './taskitem/TaskItem';
+import { MY_TASKS_URL, START_PROCESS_URL } from '../../ROUTES';
 
 import './Camunda.css';
 
 class Camunda extends React.Component {
     state = {
+        tasks: [],
         selectedTask: false,
     }
 
@@ -13,10 +18,55 @@ class Camunda extends React.Component {
         this.setState({selectedTask: task});
     }
 
+    componentDidMount() {
+        // ako je ulogovani user student, dajemo mu dugme da pokrene proces
+        const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+        const userIsStudent = (loggedInUser != null && loggedInUser.groups[0] === "student");
+        if (userIsStudent)
+            this.refs.startProcessBtn.style.display = 'block';
+
+        this.updateTasks();
+    }
+
+
+    updateTasks = () => {
+        axios.get(MY_TASKS_URL, { withCredentials: true })
+            .then(response => {
+                this.setState({ tasks: response.data });
+            })
+            .catch(response => console.log({response}));
+    }
+
+    handleStartProcess = () => {
+        axios.post(START_PROCESS_URL, {}, { withCredentials: true })
+            .then((response) => {
+                this.updateTasks();
+            })
+            .catch((response) => console.log(response));
+    }
+
+
     render() {
         return (
             <main className="Camunda">
-                <TaskList selectTask={this.selectTask} loadForm={this.loadForm} />
+            
+                <div className="TaskList">
+                    <button ref="startProcessBtn" style={{ display: 'none' }} className="start-process-btn" onClick={this.handleStartProcess}>Zapocni prijavu</button>
+                    <div className="flex-container">
+                        {this.state.tasks.map(task => {
+                            const selected = task.id === this.state.selectedTask.id;
+                            return (
+                                <TaskItem
+                                    key={task.id}
+                                    selected={selected}
+                                    task={task}
+                                    selectTask={this.selectTask} />
+                            )
+                        })}
+                    </div>
+                </div>
+
+
                 <WorkSpace task={this.state.selectedTask} />
 
             </main>
--- src/components/camunda/taskitem/TaskItem.js
+++ src/components/camunda/taskitem/TaskItem.js
@@ -3,14 +3,14 @@ import './TaskItem.css';
 
 class TaskItem extends React.Component {
     // TODO ovo sve mozda moze da se prebije u funkc komponentu. state nam ne treba 
-    state = {
-        selected: this.props.selected,
-    }
+    // state = {
+    //     selected: this.props.selected,
+    // }
 
-    selectTask = () => {
-        this.setState({selected:true});
-        this.props.selectTask(this.props.task);
-    }
+    // selectTask = () => {
+    //     this.setState({selected:true});
+    //     this.props.selectTask(this.props.task);
+    // }
 
     render() {
         const { assignee, createTime, name} = this.props.task;
@@ -21,7 +21,7 @@ class TaskItem extends React.Component {
         else
             className = "TaskItem flex-item";
         return (
-            <div className={className} onClick={this.selectTask}>
+            <div className={className} onClick={() => this.props.selectTask(this.props.task)}>
                 <h3 style={{ float: 'left' }} className="taskTitle">{name}</h3>
                 <span style={{ float: 'right ' }}>{assignee ? '♞' + assignee : 'unclaimed'}</span>
                 <div style={{ clear: 'both ' }}></div>
